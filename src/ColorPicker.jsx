371d03910a4f43cd5ee9685773efdede2c66854a
Newton712 <cle710dropbox@gmail.com>
2025-05-09 01:25:49 +0200

    Update ColorPicker.jsx

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,4 +1,3 @@
-// src/ColorPicker.jsx
 import React from 'react';
 import jaune from './assets/colors/jaune.png';
 import mauve from './assets/colors/mauve.png';
@@ -8,24 +7,26 @@ import bleu from './assets/colors/bleu.png';
 import gris from './assets/colors/gris.png';
 
 const colors = [
-  { name: 'jaune', image: jaune },
-  { name: 'mauve', image: mauve },
-  { name: 'vert', image: vert },
-  { name: 'rouge', image: rouge },
-  { name: 'bleu', image: bleu },
-  { name: 'gris', image: gris }
+  { name: 'jaune', img: jaune },
+  { name: 'mauve', img: mauve },
+  { name: 'vert', img: vert },
+  { name: 'rouge', img: rouge },
+  { name: 'bleu', img: bleu },
+  { name: 'gris', img: gris },
 ];
 
 export default function ColorPicker({ selectedColor, onChange }) {
   return (
-    <div className="flex space-x-2 items-center">
-      {colors.map(({ name, image }) => (
+    <div className="flex gap-1">
+      {colors.map((color) => (
         <button
-          key={name}
-          onClick={() => onChange(name)}
-          className={`border-2 rounded p-1 ${selectedColor === name ? 'border-blue-500' : 'border-transparent'}`}
+          key={color.name}
+          onClick={() => onChange(color.name)}
+          className={`border-2 rounded p-1 ${
+            selectedColor === color.name ? 'border-blue-500' : 'border-transparent'
+          }`}
         >
-          <img src={image} alt={name} className="w-8 h-8" />
+          <img src={color.img} alt={color.name} className="w-8 h-8 object-contain" />
         </button>
       ))}
     </div>
