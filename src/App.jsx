3d2dd0bf93ba9976f64544cfda90d0f5fe3b6227
Newton712 <cle710dropbox@gmail.com>
2025-05-08 21:55:59 +0200

    WebApp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Bienvenue dans le tournoi !</h1>
+      <p>Commence à coder ton app React ici 🚀</p>
+    </div>
+  );
+}
+
+export default App;
